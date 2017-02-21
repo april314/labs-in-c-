988121c5903931c5e1d59bf0d78ad9fd0330c8d3
april314 <nasturtiumk@yandex.com>
2017-02-21 17:32:44 +0300

    functions with cicles
    unsgned power of numbers

---
+++ power.cpp
@@ -0,0 +1,22 @@
+// определите только функцию power, где
+//    x - число, которое нужно возвести в степень
+//    p - степень, в которую нужно возвести x
+//
+#include <iostream>
+using namespace std;
+int power(int x, unsigned p);
+
+int main()
+{
+	cout << power(2,0) << endl;
+	return 0;
+}
+
+int power(int x, unsigned p) {
+    int answer = 1;
+    /* считаем answer */
+	for (unsigned i = 1; i <= p; i++) {
+		answer *= x;
+	}
+    return answer;
+}
